3f8e32a47f7c6925cfe7d6f0f6a270cbb0aef46e
VitaliyRu <vitalii.rubezhanskii@gmail.com>
2020-03-04 15:01:26 +0200

    some new feature 19

--- src/main/java/com/example/demo/controller/Controller.java
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -14,7 +14,7 @@ public class Controller {
 
     @GetMapping(value = "/api/data")
     public ResponseEntity<List<Apples>> getApples(){
-        return new ResponseEntity<>( Arrays.asList(new Apples("macintosh", 15), new Apples("antonovkas", 17)), HttpStatus.OK);
+        return new ResponseEntity<>( Arrays.asList(new Apples("macintosh", 15), new Apples("antonovkasas", 17)), HttpStatus.OK);
     }
 
 
